8ea962f056f0d5ae052cb50408b2c38e391037d2
longkpham <42297497+longkpham@users.noreply.github.com>
2021-10-05 23:08:32 +0700

    Add files via upload

---
+++ ZaloOA_API/ThirdParty_ZaloOA/ThirdParty_ZaloOA/Controllers/CustomerInfoController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ThirdParty_ZaloOA.Controllers
+{
+    [Route("api/CustomerInfo")]
+    [ApiController]
+    public class CustomerInfoController : ControllerBase
+    {
+        [HttpGet]
+        public void GetCustomer()
+        {
+            Response.StatusCode = 200;
+        } 
+    }
+}
